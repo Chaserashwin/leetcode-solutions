17f0a01dc683dcb0d82e61b92b61fe8c336385ba
Ashwin Jaiswal <92367461+Chaserashwin@users.noreply.github.com>
2024-01-18 22:51:02 +0530

    Time: 0 ms (100%), Space: 42.8 MB (41.98%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,48 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        // List<Integer> list = Arrays.asList(nums);
+        int idx1 = -1;
+        int idx2 = -1;
+        int n = nums.length;
+        // finding the breaking point
+        for(int i=n-2; i>=0; i--) {
+            if(nums[i] < nums[i+1]) {
+                idx1 = i;
+                break;
+            }
+        }
+        // if there is no breaking point
+        if(idx1 == -1) {
+            reverse(nums, 0);
+        }
+
+        // if we got any breakpoint then we need to store that in idx2 and later swap that with idx1 
+        else {
+            for(int i=n-1; i>=0; i--) {
+                if(nums[i] > nums[idx1]) {
+                    idx2 = i;
+                    break;
+                }
+            }
+            
+        swap(nums, idx1, idx2);
+        // reverse the rest 
+        reverse(nums, idx1+1);
+        }
+    }
+
+    void swap(int[] nums, int x, int y) {
+        int temp = nums[x];
+        nums[x] = nums[y];
+        nums[y] = temp;
+    }
+
+    void reverse(int[] nums, int start) {
+        int x = start;
+        int y = nums.length-1;
+        while(x < y) {
+            swap(nums, x, y);
+            x++; y--;
+        }
+    }
+}
