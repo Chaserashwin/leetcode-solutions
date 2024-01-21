ecedb7e75c54962c5e81d72a99e098e0f511bb19
Ashwin Jaiswal <92367461+Chaserashwin@users.noreply.github.com>
2024-01-21 11:54:51 +0530

    Time: 1 ms (55.15%), Space: 41.7 MB (31.51%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
         int n = mat.length;
-        boolean a = false, b = false, c = false, d = false;
+        int a = 0, b = 0, c = 0, d = 0;
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
-                if(target[i][j] == mat[j][n-1-i]) a = true;
-                if(target[i][j] == mat[n-1-i][n-1-j]) b = true;
-                if(target[i][j] == mat[n-1-j][i]) c = true;
-                if(target[i][j] == mat[i][j]) d = true;
+                if(target[i][j] == mat[j][n-1-i]) a++;
+                if(target[i][j] == mat[n-1-i][n-1-j]) b++;
+                if(target[i][j] == mat[n-1-j][i]) c++;
+                if(target[i][j] == mat[i][j]) d++;
             }
         }
-        if(a || b || c || d) return true;
+        if(a==n*n || b==n*n || c==n*n || d==n*n) return true;
         else return false;
     }
 }
