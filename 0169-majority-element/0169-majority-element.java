c701952920a0f925d1f3ea061f1bc312f888d825
Ashwin Jaiswal <92367461+Chaserashwin@users.noreply.github.com>
2024-02-06 18:47:02 +0530

    Time: 11 ms (36.34%), Space: 48.6 MB (82.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,24 +1,23 @@
+import java.util.*;
 class Solution {
     public int majorityElement(int[] nums) {
-        int count = 0;
-        int ele = 0;
-        for(int i=0; i<nums.length; i++) {
-            if(count == 0) {
-                count=1;
-                ele = nums[i];
-            }
-            else if(nums[i] == ele) {
-                count++;
-            }
-            else {
-                count--;
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++) {
+            // for checking that if the element is there or not if it is there, increase its value otherwise
+            int value = map.getOrDefault(nums[i],0);
+            map.put(nums[i], value+1);
+        }
+
+        // Searching for the value
+        for(Map.Entry<Integer,Integer> it : map.entrySet()) {
+            if(it.getValue() > (n/2)) {
+                return it.getKey();
             }
         }
-        int count1=0;
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] == ele) count1++;
-        }
-        if(count1 > nums.length/2) return ele;
-        else return -1;
+        return -1;
     }
+
 }
+
+// The getOrDefault(Object key, V defaultValue) method of Map interface, implemented by HashMap class is used to get the value mapped with specified key. If no value is mapped with the provided key then the default value is returned.
