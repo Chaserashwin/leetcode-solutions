67afa680519469e6a2d0cc086503e16783377059
Ashwin Jaiswal <92367461+Chaserashwin@users.noreply.github.com>
2025-09-14 22:22:14 +0530

    Update 0027-remove-element.js

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -4,7 +4,9 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
+    // pointer x will keep the position to be shifted
     let x = 0;
+    // pointer i will be used to check which element to shift
     for(let i=0; i<nums.length; i++) {
         if(nums[i] != val) {
             nums[x] = nums[i];
