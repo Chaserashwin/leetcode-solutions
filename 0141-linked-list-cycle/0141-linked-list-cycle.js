7851d4250ffbd2ad63a1f98b041f3cdde666e220
Ashwin Jaiswal <92367461+Chaserashwin@users.noreply.github.com>
2025-09-26 08:37:53 +0530

    Time: 58 ms (35.1%), Space: 58.5 MB (86.34%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.js
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -11,14 +11,13 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    let seenNodes = new Set();
-    let curr = head;
-    while(curr) {
-        if(seenNodes.has(curr)) {
-            return true;
-        }
-        seenNodes.add(curr);
-        curr = curr.next;
+    if(head == null) return false;
+    let slow = head;
+    let fast = head.next;
+    while(slow != fast) {
+        if(fast == null || fast.next == null) return false;
+        slow = slow.next;
+        fast = fast.next.next;
     }
-    return false;
+    return true;
 };
