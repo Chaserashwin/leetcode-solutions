26adf0d3695c74ed00c41576883a70b2b1a9131b
Ashwin Jaiswal <92367461+Chaserashwin@users.noreply.github.com>
2025-09-30 21:54:42 +0530

    Time: 1 ms (39.48%), Space: 56.6 MB (74.63%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -25,17 +25,9 @@ var mergeTwoLists = function(list1, list2) {
         newList = newList.next;
     }
     if(list1 == null) {
-        while(list2) {
-            newList.next = list2;
-            newList = newList.next;
-            list2 = list2.next;
-        }
+        newList.next = list2;
     } else{
-        while(list1) {
-            newList.next = list1;
-            newList = newList.next;
-            list1 = list1.next;
-        }
+        newList.next = list1;
     }
     return liststart.next;
 };
