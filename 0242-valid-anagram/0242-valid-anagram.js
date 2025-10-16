f1d16e506e92a74fa4f845379b6ee72fd7e455b5
Ashwin Jaiswal <92367461+Chaserashwin@users.noreply.github.com>
2025-10-16 22:33:45 +0530

    Time: 14 ms (59.44%), Space: 55.7 MB (85.75%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -7,7 +7,7 @@ var isAnagram = function(s, t) {
     if(s.length !== t.length) return false;
 
     let map = {};
-    
+
     for(let i=0; i<s.length; i++) {
         if(!map[s[i]]){
             map[s[i]] = 1;
@@ -21,7 +21,7 @@ var isAnagram = function(s, t) {
             return false;
         }
         else {
-            --map[s[i]];
+            --map[t[i]];
         }
     }
     return true;
