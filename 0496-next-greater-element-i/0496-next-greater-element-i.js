40360d062584df58d1ef791723aaddc008375b82
Ashwin Jaiswal <92367461+Chaserashwin@users.noreply.github.com>
2025-10-29 10:02:43 +0530

    Time: 4 ms (49.3%), Space: 58 MB (35.47%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -12,27 +12,20 @@ var nextGreaterElement = function(nums1, nums2) {
     stack.push(nums2[n-1]);
 
     for(let i=n-2; i>=0; i--) {
-        let top = stack[stack.length-1];
-        if(nums2[i] < top) {
-            map[nums2[i]] = top;
-            stack.push(nums2[i]);
-        }
-        else {
-            while(stack.length) {
-                if(stack[stack.length-1] > nums2[i]) {
-                    map[nums2[i]] = stack[stack.length-1];
-                    stack.push(nums2[i]);
-                    break;
-                }
-                else{
-                    stack.pop();
-                }
-            }
-            if(stack.length === 0) {
-                map[nums2[i]] = -1;
+        while(stack.length) {
+            if(stack[stack.length-1] > nums2[i]) {
+                map[nums2[i]] = stack[stack.length-1];
                 stack.push(nums2[i]);
-            } 
+                break;
+            }
+            else{
+                stack.pop();
+            }
         }
+        if(stack.length === 0) {
+            map[nums2[i]] = -1;
+            stack.push(nums2[i]);
+        } 
     }
 
     let ans = [];
