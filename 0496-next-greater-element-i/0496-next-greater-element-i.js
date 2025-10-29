4fb48db28b71315119f4db2c3a53fef1663d5b62
Ashwin Jaiswal <92367461+Chaserashwin@users.noreply.github.com>
2025-10-29 10:02:06 +0530

    Time: 5 ms (35.29%), Space: 57.8 MB (49.74%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -4,33 +4,37 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    let n = nums2.length;
-    if(n == 1) return [-1];
-    let map = {};
     let stack = [];
+    let map = {};
+    let n = nums2.length;
+
     map[nums2[n-1]] = -1;
     stack.push(nums2[n-1]);
+
     for(let i=n-2; i>=0; i--) {
         let top = stack[stack.length-1];
-        if(top > nums2[i]) {
+        if(nums2[i] < top) {
             map[nums2[i]] = top;
             stack.push(nums2[i]);
-        } 
-        else{
+        }
+        else {
             while(stack.length) {
-                if(stack[stack.length-1] < nums2[i]) {
-                    stack.pop();
-                } else{
+                if(stack[stack.length-1] > nums2[i]) {
                     map[nums2[i]] = stack[stack.length-1];
+                    stack.push(nums2[i]);
                     break;
+                }
+                else{
+                    stack.pop();
                 }
             }
             if(stack.length === 0) {
                 map[nums2[i]] = -1;
-            }
+                stack.push(nums2[i]);
+            } 
         }
-        stack.push(nums2[i]);
     }
+
     let ans = [];
     for(let i=0; i<nums1.length; i++) {
         ans.push(map[nums1[i]]);
