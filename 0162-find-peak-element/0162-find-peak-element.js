d84e93d385dae39610c24fea9e87580495bc25b5
Ashwin Jaiswal <92367461+Chaserashwin@users.noreply.github.com>
2025-11-08 06:42:10 +0530

    Time: 0 ms (100%), Space: 54.1 MB (65.94%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -7,14 +7,11 @@ var findPeakElement = function(nums) {
     let r = nums.length-1;
     while(l < r) {
         let mid = l + Math.floor((r-l)/2);
-        if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) {
-            return mid;
-        }
-        else if(nums[mid] > nums[mid+1]) {
-            r = mid;
+        if(nums[mid] < nums[mid+1]) {
+            l = mid + 1;
         }
         else{
-            l = mid + 1;
+            r = mid;
         }
     }
     return r;
