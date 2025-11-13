6f30fcdd30f35174a4f7d2eb2cb86ebbf237908e
Ashwin Jaiswal <92367461+Chaserashwin@users.noreply.github.com>
2025-11-13 12:43:15 +0530

    Time: 0 ms (100%), Space: 63.3 MB (95.88%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function(nums) {
+    let l = 0;
+    let r = nums.length - 1;
+    while(l < r) {
+        let mid = l + Math.floor((r-l)/2);
+        if(nums[mid] > nums[mid+1]) {
+            r = mid;
+        }
+        else {
+            l = mid + 1;
+        }
+    }
+    return r;
+};
