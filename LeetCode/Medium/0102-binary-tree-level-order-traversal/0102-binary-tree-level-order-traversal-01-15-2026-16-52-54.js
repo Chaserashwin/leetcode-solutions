7a5237d3efd7db84c54716f3c96ea5972129fafb
Ashwin Jaiswal <92367461+Chaserashwin@users.noreply.github.com>
2026-01-15 16:52:54 +0530

    0102-binary-tree-level-order-traversal 0 ms (100%) 57.4 MB (92.4%) Medium 01-15-2026

---
+++ LeetCode/Medium/0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal-01-15-2026-16-52-54.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {
+    if(!root) return [];
+    let q = [root];
+    let ans = [];
+    while(q.length) {
+        let levelarr = [];
+        let levelsize = q.length;
+        for(let i=0; i<levelsize; i++) {
+            let curr = q.shift();
+            levelarr.push(curr.val);
+            curr.left && q.push(curr.left);
+            curr.right && q.push(curr.right);
+        }
+        ans.push(levelarr);
+    }
+    return ans;
+};
