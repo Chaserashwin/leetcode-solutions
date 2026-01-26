dbe337cea31076f15a40883af8c28d73a6f368a3
Ashwin Jaiswal <92367461+Chaserashwin@users.noreply.github.com>
2026-01-26 23:23:43 +0530

    0112-path-sum 0 ms (100%) 57.3 MB (82.18%) Easy 01-26-2026

---
+++ LeetCode/Easy/0112-path-sum/0112-path-sum-01-26-2026-23-23-42.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, targetSum) {
+    //base cases
+    if(!root) return false;
+    if(!root.left && !root.right) {
+        return root.val === targetSum;
+    }
+    //main logic
+    let leftSubTreeHasPathSum = hasPathSum(root.left, targetSum-root.val);
+    let rightSubTreeHasPathSum = hasPathSum(root.right, targetSum-root.val);
+    return leftSubTreeHasPathSum || rightSubTreeHasPathSum;
+};
